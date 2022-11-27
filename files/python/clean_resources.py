70abd7c9d9b2f0778cb8a6c43edb6ad68ad15745
swahyu <swahyu1412@gmail.com>
2022-11-27 15:22:25 +0700

    modify python script

--- files/python/clean_resources.py
+++ files/python/clean_resources.py
@@ -1,20 +1,24 @@
 import boto3
 
-def remove_ami():
-  my_ami = ec2.describe_images(Owners=['self'])
+ec2 = boto3.resource("ec2")
+
+regions = []
+
+for region in ec2.meta.client.describe_regions()['Regions']:
+  regions.append(region['RegionName'])
+
+
+for region in regions:
+  ec2_client = boto3.client("ec2", region_name=region)
+  ec2 = boto3.resource("ec2", region_name=region)
+  
+  my_ami = ec2_client.describe_images(Owners=['self'])
 
   for ami in my_ami['Images']:
     ami_id = ami['ImageId']
-    ec2.deregister_image(ImageId=ami_id)
+    ec2_client.deregister_image(ImageId=ami_id)
 
-def remove_snapshot():
   snapshots = ec2.snapshots.filter(OwnerIds=['self'])
 
   for snapshot in snapshots:
-    snapshot.delete()    
-
-if __name__ == '__main__':
-  ec2 = boto3.client("ec2", region_name='ap-southeast-1')
-  remove_ami
-  ec2 = boto3.resource("ec2", region_name='ap-southeast-1')
-  remove_snapshot
+    snapshot.delete()
