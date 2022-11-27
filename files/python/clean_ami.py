b6ad46fcea32884ea819be277e345f8a8e7c1dae
swahyu <swahyu1412@gmail.com>
2022-11-27 13:54:46 +0700

    Add specify region

--- files/python/clean_ami.py
+++ files/python/clean_ami.py
@@ -1,8 +1,12 @@
 import boto3
 
-ec2 = boto3.client("ec2")
-my_ami = ec2.describe_images(Owners=['self'])
+def remove_ami():
+  my_ami = ec2.describe_images(Owners=['self'])
 
-for ami in my_ami['Images']:
-  ami_id = ami['ImageId']
-  ec2.deregister_image(ImageId=ami_id)
+  for ami in my_ami['Images']:
+    ami_id = ami['ImageId']
+    ec2.deregister_image(ImageId=ami_id)
+
+if __name__ == '__main__':
+  ec2 = boto3.client("ec2", region_name='ap-southeast-1')
+  remove_ami
--- files/python/clean_snapshot.py
+++ files/python/clean_snapshot.py
@@ -1,7 +1,12 @@
 import boto3
 
-ec2 = boto3.resource("ec2")
-snapshots = ec2.snapshots.filter(OwnerIds=['self'])
+def remove_snapshot():
+  ec2 = boto3.resource("ec2")
+  snapshots = ec2.snapshots.filter(OwnerIds=['self'])
 
-for snapshot in snapshots:
-  snapshot.delete()
+  for snapshot in snapshots:
+    snapshot.delete()
+
+if __name__ == '__main__':
+  ec2 = boto3.resource("ec2", region_name="ap-southeast-1")
+  remove_snapshot
