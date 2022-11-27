1e3a29ee65e50fe50d7c026aefc2bceb45e512c1
swahyu <swahyu1412@gmail.com>
2022-11-27 11:13:28 +0700

    Syntax adjusted

--- files/packer/bash_script/requirements_install.sh
+++ files/packer/bash_script/requirements_install.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install nginx -y
 
 echo "Installing apache..."
-sudo apt-get install apache2 -y
+sudo apt-get install apache2 apache2-bin apache2-utils mime-support ssl-cert -y
 
 sudo apt-get -f install 
 
