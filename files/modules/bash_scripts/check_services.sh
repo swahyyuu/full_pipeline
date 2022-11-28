82e304cd1c08c4a8a882aa6f429fd46f30bdcb40
swahyu <swahyu1412@gmail.com>
2022-11-28 18:59:06 +0700

    test 21

--- files/modules/bash_scripts/check_services.sh
+++ files/modules/bash_scripts/check_services.sh
@@ -15,12 +15,14 @@ function check_apache() {
   fi
 }
 
+echo "Apache being set up..."
+sudo systemctl disable apache2
+
 echo "Setting up inside the server..."
 sudo sed -i 's/80/2000/' /etc/apache2/sites-available/000-default.conf
 sudo ufw allow 2000
 
-echo "Apache being set up..."
-sudo systemctl disable apache2
+echo "Allow Apache..."
 sudo ufw allow 'Apache'
 sudo systemctl enable apache2
 check_apache
@@ -30,7 +32,10 @@ sudo systemctl stop nginx
 sudo ufw allow 'Nginx HTTP'
 check_nginx
 
+echo "curl nginx server..."
 curl localhost
+
+echo "curl apache server..."
 curl localhost:2000
 
 cat /etc/apache2/sites-available/000-default.conf
