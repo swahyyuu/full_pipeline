fc0775e1e2740f1b10ad0ce00a30efed0a3a339c
swahyu <swahyu1412@gmail.com>
2022-11-28 17:03:31 +0700

    test 14

--- files/modules/bash_scripts/check_kms.sh
+++ files/modules/bash_scripts/check_kms.sh
@@ -2,7 +2,8 @@
 
 function check_kms() {
   aws kms list-aliases --query 'Aliases[].AliasName' | grep my-terra-kms
-  if [[ $? -eq 0 ]]; then 
+  if [[ $? -eq 0 ]]; then
+    cd modules/aws_kms/
     rm main.tf && mv main.tf.kms main.tf 
     rm outputs.tf && mv outputs.tf.kms outputs.tf
   fi
