f58c57ae6a486fad3b095c1e665d3720b8548539
swahyu <swahyu1412@gmail.com>
2022-11-28 17:26:20 +0700

    test 16

--- files/modules/bash_scripts/check_kms.sh
+++ files/modules/bash_scripts/check_kms.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 
 function check_kms() {
-  aws kms list-aliases --query 'Aliases[].AliasName' | grep my-terra-kms
+  aws kms describe-key --key-id alias/my-terra-kms | grep "DeletionDate"
   if [[ $? -eq 0 ]]; then
     cd modules/aws_kms/
     rm main.tf && mv main.tf.kms main.tf 
     rm outputs.tf && mv outputs.tf.kms outputs.tf
+    rm variables.tf && mv variables.tf.kms variables.tf
   fi
 }
 
