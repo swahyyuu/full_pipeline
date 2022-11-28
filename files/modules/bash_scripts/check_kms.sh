e5e9ff347fa60adb3c755084938ff8b02cb694ce
swahyu <swahyu1412@gmail.com>
2022-11-28 17:47:22 +0700

    test 17

--- files/modules/bash_scripts/check_kms.sh
+++ files/modules/bash_scripts/check_kms.sh
@@ -4,9 +4,7 @@ function check_kms() {
   aws kms describe-key --key-id alias/my-terra-kms | grep "DeletionDate"
   if [[ $? -eq 0 ]]; then
     cd modules/aws_kms/
-    rm main.tf && mv main.tf.kms main.tf 
-    rm outputs.tf && mv outputs.tf.kms outputs.tf
-    rm variables.tf && mv variables.tf.kms variables.tf
+    sed -i '4s/my-terra-kms/remove-delayed' variables.tf
   fi
 }
 
