64512a5fea37ceb3f0acc4e55667f4f7fcab0533
swahyu <swahyu1412@gmail.com>
2022-11-28 16:05:46 +0700

    test 7

--- files/modules/bash_scripts/check_kms.sh
+++ files/modules/bash_scripts/check_kms.sh
@@ -1,12 +1,11 @@
-#!/bin/bash 
-function check_kms(kms_name) {
-  aws kms list-aliases --query 'Aliases[]/AliasName' | grep $kms_name
+#!/bin/bash
+
+function check_kms() {
+  aws kms list-aliases --query 'Aliases[]/AliasName' | grep my-terra-kms
   if [[ $? -gt 0 ]]; then 
-    continue
-  else
     sudo sed -i '7s/resource/data' ./modules/aws_kms/main.tf
     sudo sed -i '3s/aws_kms_alias/data.aws_kms_alias' ./modules/aws_kms/outputs.tf
   fi
 }
 
-check_kms($1)
+check_kms()
