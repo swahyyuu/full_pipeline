a222431844a20d1a89cd288833c55a7ea637d1b4
swahyu <swahyu1412@gmail.com>
2022-11-29 14:11:38 +0700

    test 25

--- files/packer/bash_script/requirements_install.sh
+++ files/packer/bash_script/requirements_install.sh
@@ -12,9 +12,12 @@ sudo apt-get -f install
 
 echo "Create folder"
 sudo mkdir /home/wahyu
+ls -al /home
 
 echo "Configuring Nginx port..."
 sudo sed -i 's/80/8080/' /etc/nginx/nginx.conf
+cat /etc/nginx/nginx.conf
 
 echo "Configuring Apache port..."
-sudo sed -i 's/80/2000' /etc/apache2/sites-available/000-default.conf
+sudo sed -i 's/80/2000/' /etc/apache2/sites-available/000-default.conf
+cat /etc/apache2/sites-available/000-default.conf
