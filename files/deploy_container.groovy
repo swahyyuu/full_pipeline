6209e17905428a6e935669f8feb75d67b2eed1ca
swahyu <swahyu1412@gmail.com>
2022-11-30 16:01:20 +0700

    test 47

--- files/deploy_container.groovy
+++ files/deploy_container.groovy
@@ -1,6 +1,11 @@
 def deploy() {
-  echo "This is your username for DockerHub account : ${params.dockerhub_user}"
-  sh "docker run -d -p 5002:80 --name flask_from_jenkins ${params.dockerhub_user}/jenkins:3.1"
+  try {
+    echo "This is your username for DockerHub account : ${params.dockerhub_user}"
+    sh "docker run -d -p 5002:80 --name flask_from_jenkins ${params.dockerhub_user}/jenkins:3.1"
+  } catch(err) {
+    sh "docker rm -f flask_from_jenkins"
+    sh "docker run -d -p 5002:80 --name flask_from_jenkins ${params.dockerhub_user}/jenkins:3.1"
+  }
 }
 
 return this
